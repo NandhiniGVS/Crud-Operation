9743e8eb6f8aadda18eb3997820ff6e71b52f398
Nandhini Shanmugam <147997081+NandhiniGVS@users.noreply.github.com>
2024-01-02 10:02:22 -0500

    Delete Crud-Backend directory

--- Crud-Backend/src/main/java/com/Curd/operation/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.Curd.operation.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.Curd.operation.model.User;
-
-public interface UserRepository extends JpaRepository <User,Long>{
-
-}
